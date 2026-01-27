ff60b4ea4deeab60e4d2dea518364ff048ba8dbd
Sego87 <michelesegolini.jobs@gmail.com>
2026-01-27 19:36:55 +0100

    debug: exclude test-static from proxy matcher

--- proxy.ts
+++ proxy.ts
@@ -14,5 +14,5 @@ export default function proxy(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
+    matcher: ['/((?!api|_next|_vercel|test-static|.*\\..*).*)']
 };
